459add665939e4558613f251a66c13ec5be14b42
Will <yh@fir.im>
2017-04-21 15:12:41 +0800

    修改环境变量

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 # This step will use fir-cli to publish your app
 #
 #   Variables used:
-#     $FIR_API_TOKEN
-#     $FIR_CHANGELOG
+#     $FLOW_FIR_API_TOKEN
+#     $FLOW_FIR_CHANGELOG
 #     $FIR_APP_PATH
 #
 #   Outputs:
@@ -51,14 +51,14 @@ function filter()
   done
 }
 if [[ -z $FIR_APP_PATH ]]; then
-  FLOW_BUILD_APP_PATH=$(filter "release" 2>&1)
-  if [[ -z $FLOW_BUILD_APP_PATH ]]; then
-    FLOW_BUILD_APP_PATH=$(filter "debug" 2>&1)
-    if [[ -z $FLOW_BUILD_APP_PATH ]]; then
-      FLOW_BUILD_APP_PATH=$(filter "debug_unaligned" 2>&1)
-      if [[ -z $FLOW_BUILD_APP_PATH ]]; then
-        FLOW_BUILD_APP_PATH=$(filter "random" 2>&1)
-        if [[ -z $FLOW_BUILD_APP_PATH ]]; then
+  FLOW_FIR_APP_PATH=$(filter "release" 2>&1)
+  if [[ -z $FLOW_FIR_APP_PATH ]]; then
+    FLOW_FIR_APP_PATH=$(filter "debug" 2>&1)
+    if [[ -z $FLOW_FIR_APP_PATH ]]; then
+      FLOW_FIR_APP_PATH=$(filter "debug_unaligned" 2>&1)
+      if [[ -z $FLOW_FIR_APP_PATH ]]; then
+        FLOW_FIR_APP_PATH=$(filter "random" 2>&1)
+        if [[ -z $FLOW_FIR_APP_PATH ]]; then
           echo "WARNING: can't find .apk file"
           exit 1
         fi
@@ -66,25 +66,25 @@ if [[ -z $FIR_APP_PATH ]]; then
     fi
   fi
 else
-  FLOW_BUILD_APP_PATH=$FLOW_BUILD_PROJECT_PATH/$FIR_APP_PATH
+  FLOW_FIR_APP_PATH=$FLOW_BUILD_PROJECT_PATH/$FIR_APP_PATH
 fi
 
-echo $FLOW_BUILD_APP_PATH
+echo $FLOW_FIR_APP_PATH
 
-if [[ -z $FIR_API_TOKEN ]]; then
-  echo "WARNING: the [FIR_API_TOKEN] not exist..."
+if [[ -z $FLOW_FIR_API_TOKEN ]]; then
+  echo "WARNING: the [FLOW_FIR_API_TOKEN] not exist..."
   exit 1
 fi
 
-if [[ -f $FLOW_BUILD_APP_PATH ]]; then
-  echo "FLOW_BUILD_APP_PATH exist"
+if [[ -f $FLOW_FIR_APP_PATH ]]; then
+  echo "FLOW_FIR_APP_PATH exist"
 else
-  echo "WARNING: the [FLOW_BUILD_APP_PATH] not exist..."
+  echo "WARNING: the [FLOW_FIR_APP_PATH] not exist..."
   exit 1
 fi
 
-if [[ -z $FIR_CHANGELOG ]]; then
-  FIR_CHANGELOG="$(date +%Y-%m-%d:%H:%M:%S): build from flow.ci"
+if [[ -z $FLOW_FIR_CHANGELOG ]]; then
+  FLOW_FIR_CHANGELOG="$(date +%Y-%m-%d:%H:%M:%S): build from flow.ci"
 fi
 
 echo -e "ruby -v"
@@ -107,8 +107,8 @@ echo "fir-cli version"
 fir --version
 
 set -e
-echo "fir publish [FLOW_BUILD_APP_PATH] -T [FIR_API_TOKEN] -c [FIR_CHANGELOG]"
-#fir publish $FLOW_BUILD_APP_PATH -c $FIR_CHANGELOG -T $FIR_API_TOKEN
+echo "fir publish [FLOW_FIR_APP_PATH] -T [FLOW_FIR_API_TOKEN] -c [FLOW_FIR_CHANGELOG]"
+#fir publish $FLOW_FIR_APP_PATH -c $FLOW_FIR_CHANGELOG -T $FLOW_FIR_API_TOKEN
 
 echo "
 require 'fir'
@@ -116,7 +116,7 @@ require 'open3'
 logfile = STDOUT
 FIR.logger       = Logger.new(logfile)
 FIR.logger.level =  Logger::INFO
-FIR.publish(\"${FLOW_BUILD_APP_PATH}\", {token: \"${FIR_API_TOKEN}\", changelog: \"${FIR_CHANGELOG}\"})
+FIR.publish(\"${FLOW_FIR_APP_PATH}\", {token: \"${FLOW_FIR_API_TOKEN}\", changelog: \"${FLOW_FIR_CHANGELOG}\"})
 info = FIR.fetch_app_info
 url = \"http://fir.im/#{info[:short]}?release_id=#{info[:master_release_id]}\"
 File.open(\"/tmp/fir-cli.storage\", \"w+\") do |file|
